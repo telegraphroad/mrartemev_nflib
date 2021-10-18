6701b2773e886441814e5da0988db3b8e1b62707
telegraphroad <saba.amiri@outlook.com>
2021-10-18 14:41:54 +0200

    conv id added

--- flows/glow.py
+++ flows/glow.py
@@ -47,11 +47,14 @@ class Invertible1x1Conv(nn.Module):
     As introduced in Glow paper.
     """
 
-    def __init__(self, dim):
+    def __init__(self, dim, init='random'):
         super().__init__()
         self.register_buffer('placeholder', torch.randn(1))
         self.dim = dim
-        Q = torch.nn.init.orthogonal_(torch.randn(dim, dim))
+        if init == 'random':
+            Q = torch.nn.init.orthogonal_(torch.randn(dim, dim))
+        elif init == 'identity':
+            Q = torch.nn.init.orthogonal_(torch.ones(dim, dim))
         P, L, U = torch.lu_unpack(*Q.lu())
         self.P = nn.Parameter(P, requires_grad=False)  # remains fixed during optimization
         self.L = nn.Parameter(L, requires_grad=True)  # lower triangular portion
@@ -77,3 +80,4 @@ class Invertible1x1Conv(nn.Module):
         x = z @ W_inv
         log_det = -torch.sum(torch.log(torch.abs(self.S)))
         return x, log_det
+
