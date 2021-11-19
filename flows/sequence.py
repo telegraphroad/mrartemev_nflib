6a0dd50e90e598e964078c254a500808a8ea979a
Saba Amiri <saba.amiri@outlook.com>
2021-11-19 13:46:10 +0100

    Update sequence.py

--- flows/sequence.py
+++ flows/sequence.py
@@ -301,3 +301,66 @@ class NormalizingFlowModelGGD(nn.Module):
         x, _ = self.inverse(z, context=context)
         return x
 
+class NormalizingFlowModelMVN(nn.Module):
+    """ A Normalizing Flow Model is a (prior, flow) pair """
+
+    def __init__(self,rep_sample, flows,loc,cov):
+        super().__init__()
+        self.register_buffer('placeholder', torch.randn(1))
+        #self.prior = prior
+        self.flows = nn.ModuleList(flows)
+        self._dim = None
+        self._rep_sample = rep_sample
+        self.loc = nn.Parameter(torch.zeros(())+loc)
+        self.cov = nn.Parameter(torch.eye(())+cov)
+        
+        self.loc.requires_grad = True
+        self.cov.requires_grad = True
+        
+        
+        self.prior = MultivariateNormal(self.loc, self.cov)
+        
+    def forward(self, x, context=None):
+        m, self._dim = x.shape
+        log_det = torch.zeros(m, device=self.placeholder.device)
+        for flow in self.flows:
+            x, ld = flow.forward(x, context=context)
+            log_det += ld
+        z, prior_logprob = x, self.prior.log_prob(x)
+        return z, prior_logprob, log_det
+
+    def inverse(self, z, context=None):
+        if len(z.shape)>2:
+            z = z.squeeze()
+        m, _ = z.shape
+        log_det = torch.zeros(m, device=self.placeholder.device)
+        for flow in self.flows[::-1]:
+            z, ld = flow.inverse(z, context=context)
+            log_det += ld
+        x = z
+        return x, log_det
+
+    def log_prob(self, x):
+        _, prior_logprob, log_det = self.forward(x)
+        return prior_logprob + log_det
+
+    def sample(self, num_samples, context=None):
+        if type(self.prior) == torch.distributions.multivariate_normal.MultivariateNormal:
+          if self._rep_sample:
+            z = self.prior.rsample((num_samples,)).to(self.placeholder.device)
+            
+          else:
+            z = self.prior.sample((num_samples,)).to(self.placeholder.device)
+            
+          #print('mvn')
+        else:
+          if self._rep_sample:
+            z = self.prior.rsample((num_samples,self._dim)).to(self.placeholder.device)
+            
+          else:
+            z = self.prior.sample((num_samples,self._dim)).to(self.placeholder.device)
+            
+          #print('ggd')
+        x, _ = self.inverse(z, context=context)
+        return x
+
