ec82d728d309bab92f29845fbcc305aada0b4d0a
Saba Amiri <saba.amiri@outlook.com>
2021-12-03 10:02:34 +0100

    Update __init__.py

--- flows/__init__.py
+++ flows/__init__.py
@@ -1,4 +1,4 @@
-from .sequence import InvertiblePermutation, NormalizingFlowModel, NormalizingFlowModelGGD,NormalizingFlowModelMVN,NormalizingFlowModelMVGGD
+from .sequence import InvertiblePermutation, NormalizingFlowModel, NormalizingFlowModelGGD,NormalizingFlowModelMVN,NormalizingFlowModelMVGGD,NormalizingFlowModelTMVN
 from .affine import AffineConstantFlow, AffineHalfFlow, MAF, IAF
 from .glow import ActNorm, Invertible1x1Conv
 from .spline import NSF_AR
